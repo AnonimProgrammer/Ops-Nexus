8fd8b057e64b3088b0f33bf6d5a65e05fb86974f
AnonimProgrammer <zephyr11ff@gmail.com>
2025-12-17 23:36:12 +0400

    Project root init.

---
+++ src/test/java/com/nexus/opsnexus/OpsNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexus.opsnexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpsNexusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
